698c6a529b55e3b6f1c42d71128df00906342144
Imnotcooking <chen615711111@gmail.com>
2026-04-28 13:19:55 +0100

    BUGFIX: Restored the 9-feature state space for the AI Commander

--- master_daily_cron.py
+++ master_daily_cron.py
@@ -46,10 +46,17 @@ df_pd = raw_data.reset_index()
 df_pd.columns = ['Date', 'Close']
 lf = pl.LazyFrame(df_pd)
 
+# Replace the old math block with this:
 lf = lf.with_columns([
     (pl.col("Close") / pl.col("Close").shift(1)).log().alias("Ret_1D"),
+    (pl.col("Close") / pl.col("Close").shift(5)).log().alias("Ret_5D"),
     (pl.col("Close") / pl.col("Close").shift(20)).log().alias("Ret_20D"),
+    (pl.col("Close") / pl.col("Close").shift(60)).log().alias("Ret_60D"),
     ((pl.col("Close") / pl.col("Close").shift(1)).log().rolling_std(window_size=20) * (252 ** 0.5)).alias("Vol_20D"),
+    ((pl.col("Close") / pl.col("Close").shift(1)).log().rolling_std(window_size=60) * (252 ** 0.5)).alias("Vol_60D"),
+    ((pl.col("Close") / pl.col("Close").rolling_mean(window_size=20)) - 1.0).alias("Dist_SMA_20"),
+    ((pl.col("Close") / pl.col("Close").rolling_mean(window_size=50)) - 1.0).alias("Dist_SMA_50"),
+    ((pl.col("Close") / pl.col("Close").rolling_mean(window_size=200)) - 1.0).alias("Dist_SMA_200"),
 ]).drop_nulls()
 
 latest_state = lf.collect().tail(1)
